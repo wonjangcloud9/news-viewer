da9abbea479b8c9e136e95b9e8b4f1203dd0d074
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-12 20:32:18 +0900

    custom hook

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
 import qs from 'qs';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -25,39 +26,31 @@ const sampleArticle = {
 };
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const KEY = process.env.REACT_APP_NEWS_API_KEY;
-  useEffect(() => {
-    // async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = qs.stringify({
-          country: 'kr',
-          category: category === 'all' ? '' : category,
-          apiKey: KEY,
-        });
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?${query}`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = qs.stringify({
+      category: category === 'all' ? '' : category,
+      country: 'kr',
+    });
+    return axios.get(`https://newsapi.org/v2/top-headlines?${query}`, {
+      headers: {
+        Authorization: process.env.REACT_APP_NEWS_API_KEY,
+      },
+    });
   }, [category]);
 
   // 대기 중일 때
   if (loading) {
     return <NewsListBlock>대기 중...</NewsListBlock>;
   }
-  // 아직 articles 값이 설정되지 않았을 때
-  if (!articles) {
+
+  if (!response) {
     return null;
   }
+
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+  const { articles } = response.data;
 
   return (
     <NewsListBlock>
